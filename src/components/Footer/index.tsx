43fb709a61719489dc50a473ef022e3426085a87
Vinicius Moralis <vinicius_moralis@hotmail.com>
2021-03-30 20:28:39 -0300

    finalizando footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import { Container, FooterText, ContainerBrands, RdSmallLogo, BrandLogo } from './styles';
 
-import { Container, FooterText, ContainerBrands, FourbioLogo, DrogaRaiaLogo, DrogasilLogo, FarmasilLogo, UniversLogo, RdSmallLogo } from './styles';
+import fourbioLogo from '../../assets/4bio.png';
+import drogaRaiaLogo from '../../assets/drogaraia.png';
+import drogasilLogo from '../../assets/drogasil.png';
+import farmasilLogo from '../../assets/farmasil.png';
+import universLogo from '../../assets/univers.png';
 
 export default function Footer(){
   return(
@@ -10,11 +15,11 @@ export default function Footer(){
       </FooterText>
 
       <ContainerBrands>
-        <DrogaRaiaLogo/>
-        <DrogasilLogo/>
-        <FarmasilLogo/>
-        <UniversLogo/>
-        <FourbioLogo/>
+        <BrandLogo imageUrl={drogaRaiaLogo} ></BrandLogo>
+        <BrandLogo imageUrl={drogasilLogo} ></BrandLogo>
+        <BrandLogo imageUrl={farmasilLogo} ></BrandLogo>
+        <BrandLogo imageUrl={universLogo} ></BrandLogo>
+        <BrandLogo imageUrl={fourbioLogo} ></BrandLogo>
       </ContainerBrands>
 
       <RdSmallLogo/>
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,16 +1,17 @@
 import styled from 'styled-components';
-import fourbioLogo from '../../assets/4bio.png';
-import drogaRaiaLogo from '../../assets/drogaraia.png';
-import drogasilLogo from '../../assets/drogasil.png';
-import farmasilLogo from '../../assets/farmasil.png';
-import universLogo from '../../assets/univers.png';
+
 import rdSmallLogo from '../../assets/rd-small.png';
+
+interface BrandLogoProps {
+  imageUrl: any;
+}
 
 export const Container = styled.div`
   display: flex;
   flex-direction: row;
   height: 80px;
   justify-content: space-between;
+  align-items: center;
 `
 
 export const FooterText = styled.h2`
@@ -27,43 +28,23 @@ export const ContainerBrands = styled.div`
   margin-right: 280px;
 `
 
-export const FourbioLogo = styled.div`
-  background-image: url(${fourbioLogo});
-  background-repeat: no-repeat;
-  height: 40px;
-  width: 70px;
-`
-
-export const DrogaRaiaLogo = styled.div`
-  background-image: url(${drogaRaiaLogo});
-  background-repeat: no-repeat;
-  height: 40px;
-  width: 75px;
-`
-export const DrogasilLogo = styled.div`
-  background-image: url(${drogasilLogo});
-  background-repeat: no-repeat;
-  height: 40px;
-  width: 90px;
-`
-
-export const FarmasilLogo = styled.div`
-  background-image: url(${farmasilLogo});
-  background-repeat: no-repeat;
-  height: 40px;
-  width: 80px;
-`
-
-export const UniversLogo = styled.div`
-  background-image: url(${universLogo});
-  background-repeat: no-repeat;
-  height: 40px;
-  width: 75px;
-`
+// export const UniversLogo = styled.div`
+//   background-image: url(${universLogo});
+//   background-repeat: no-repeat;
+//   height: 40px;
+//   width: 75px;
+// `
 
 export const RdSmallLogo = styled.div`
   background-image: url(${rdSmallLogo});
   background-repeat: no-repeat;
   height: 40px;
   width: 75px;
+`
+
+export const BrandLogo = styled.div<BrandLogoProps>`
+  background-image: ${(props) => `url(${props.imageUrl})`};
+  background-repeat: no-repeat;
+  height: 40px;
+  width: 90px;
 `
